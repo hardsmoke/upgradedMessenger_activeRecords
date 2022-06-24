741f67beee46b86585b188c2238540a42cd2b714
Aleksandr Martynov <77721717+hardsmoke@users.noreply.github.com>
2022-06-24 13:45:13 +1000

    Update Message.php

--- src/Model/Entity/Message.php
+++ src/Model/Entity/Message.php
@@ -6,11 +6,13 @@ class Message
 {
     private string $username;
     private string $message;
+    private PDO $connection;
 
     public function __construct(string $username, string $message)
     {
         $this->username = $username;
         $this->message = $message;
+        $this->connection = new PDO("mysql:host=localhost;dbname=messages", 'root', 'aGo90nPIi');
     }
 
     public function GetUsername() : string
@@ -21,5 +23,44 @@ class Message
     public function GetMessage() : string
     {
         return $this->message;
+    }
+    
+    public function Add(Message $message) : bool
+    {
+        $messageText = $message->GetMessage();
+        $username = $message->GetUsername();
+
+        $query = $this->connection->prepare("INSERT INTO messages(message, username, datetime) VALUES (:messageText, :username, NOW()))";
+        return $query->execute(['messageText' => $messageText, 'username' => $username]);
+    }
+	
+    public function Delete(int $id) : bool
+    {
+	    $query = $this->connection->prepare('DELETE FROM messages WHERE id = :id');
+        return $query->execute();
+    }
+
+    public function GetAll() : array
+    {
+        $query = $this->connection->prepare('SELECT message, username FROM messages');
+	
+	$query->execute();
+	$data = $query->fetchAll();
+        $messages = array();
+        foreach ($data as $array){
+            $messages[] = new Message($array['message'], $array['username']);
+        }
+	    
+        return $messages;
+    }
+	
+    public function GetById(int $id) : Message
+    {
+	$query = $this->pdo->prepare('SELECT * FROM messages WHERE id = :id');
+	    
+        $query->execute(['id' => $id]);
+        $data = $query->fetch();
+	
+        return new Message($data['message'], $data['username']);
     }
 }
